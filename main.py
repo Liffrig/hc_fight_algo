92f84ce8bb0d8ac300b01ed1a5770e7c77d06357
Liffrig <michal.pavlovs@gmail.com>
2022-10-06 01:48:20 +0200

    brut force to check all possib

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
+from ast import While
+from operator import indexOf
+
+
 class Player:
     attack_types_values = {"N": 10, "H":20}
 
-    def __init__(self, attack_stat, stamina):
-        self.attack_stat = attack_stat
-        self.base_stamina = stamina
+    def __init__(self, stamina):
+        self.attack_stat = 11
+        self.base_stamina = 100
         self.stamina = stamina
     
     def calculate_attack(self,type):
@@ -38,25 +42,83 @@ class Player:
             return 0
 
         
+class State:
+    def __init__(self,path,round, ene_hp, pl_stam) -> None:
+        self.path = path
+        self.round = round
+        self.ene_hp = ene_hp
+        self.pl_stam = pl_stam
+    def print_all(self):
+        print(self.path ,self.round, self.ene_hp, self.pl_stam)
+
+
+
+
+
+
+
+
+atks = ["N","H"]
+states_list = []
+st0 = State("0-",0,110,100)
+states_list.append(st0)
+
+def remove_done(gen):
+    for i in reversed(states_list):
+        if i.ene_hp < 0 or i.round < gen:
+            inx = indexOf(states_list,i)
+            states_list.pop(inx)
+
+current_generation = -1
+
+while len(states_list) > 0:
+    for st in states_list:
+        for at in atks:
+            player = Player(st.pl_stam)
+            new_gen = st.round + 1
+            new_ene_hp = st.ene_hp - player.attack(at)
+            nw_state = State(st.path + f"{at}-" ,new_gen,new_ene_hp, player.stamina)
+            nw_state.print_all()
+
+            if nw_state.ene_hp <0:
+                print("---ENDS HERE---")
+
+
+            if nw_state.ene_hp > 0:
+                states_list.append(nw_state)
     
+        current_generation +=1
+        remove_done(current_generation)
 
 
 
-player1 = Player(11,100)
-enemy_health = 110
 
-round_counter = 1
 
-while enemy_health > 0:
-    print(f"*** ROUND {round_counter} ***")
 
-    if enemy_health - player1.calculate_attack("N") <= 0:
-        enemy_health -= player1.attack("N")
-    else:
-        enemy_health -= player1.attack("H")
 
-    print(f"Enemy HP >>> {enemy_health}")
-    round_counter += 1
+
+
+
+
+
+
+
+
+# while enemy_health > 0:
+
+
+
+#     print(" ")
+#     print(f"*** ROUND {round_counter} ***")
+
+
+#     if enemy_health - player1.calculate_attack("H") <= 0:
+#         enemy_health -= player1.attack("H")
+#     else:
+#         enemy_health -= player1.attack("N")
+
+#     print(f"Enemy HP >>> {enemy_health}")
+#     round_counter += 1
 
 
 
